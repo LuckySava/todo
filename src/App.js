1ab0c8ea8aabbf0accffb711e99b19a640c4958a
LuckySava <vivatukraine@yahoo.com>
2021-08-13 18:36:36 +0300

    add del functionality

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import styles from './App.module.scss';
 import Input from './components/UI/Input/Input';
 import Button from './components/UI/Button/Button'
 import { useEffect, useState } from 'react';
+import Todo from './components/Todo/Todo';
+// import {ReactComponent as EditBtn} from './assets/svg/edit-button.svg';
+// import {ReactComponent as SaveBtn} from './assets/svg/diskette.svg';
+// import {ReactComponent as DoneBtn} from './assets/svg/approved.svg';
+// import {ReactComponent as DeleteBtn} from './assets/svg/delete.svg';
 
 function App() {
 
@@ -21,20 +26,34 @@ const onButtonClick = (e) => {
 
 useEffect(() => {
   setValue(value)
-},[value])
+},[value, task])
 
+const handleDelete = (id) => {
+  console.log(id);
+  setTasks([...task.filter((task, ind) => ind+1 !== id)])
+}
 
   return (
     <>
     <section className={styles.app}>
       <div className={styles.container}>
+
+
         <form className={styles.main_form}>
           <Input value={value} onChange={getInputValue} type='text' placeholder="Enter your task here"/>
           <Button onClick={onButtonClick}>Create Task</Button>
         </form>
 
-        <div className="tasks">
-          
+        {
+          task.length
+          ? <h2 className={`${styles.todo_title} ${styles.todo_header}`}>Your Tasks:</h2>
+          : <h2 className={`${styles.todo_title_empty} ${styles.todo_header}`}>Nothing to do!</h2>
+        }
+
+        <div className={styles.tasks}>
+      
+          {task.map((item, ind) => <Todo del={handleDelete} number={ind+1} key={ind} body={item} />)}
+
         </div>
       </div>
     </section>
